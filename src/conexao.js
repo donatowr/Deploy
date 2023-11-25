babbe36a1099a9b5a0d16b583921e5dfb3a27e6b
Wellington Donato <dev.donatello3@gmail.com>
2023-11-25 15:26:45 -0300

    upconect

--- src/conexao.js
+++ src/conexao.js
@@ -2,11 +2,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : 'cubos-teste.cwhglp5zhx80.us-east-1.rds.amazonaws.com',
+      host : 'cubosdb.cwhglp5zhx80.us-east-1.rds.amazonaws.com',
       port : 5432,
       user : 'postgres',
       password : 'don158692',
-      database : 'myDatabase',
+      database : 'cubos_app',
       ssl: { rejectUnauthorized: false },
     }
   });
