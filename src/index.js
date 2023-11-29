b81d396a7afe844e1ec25f4bf5616b6639a76a8d
Wellington Donato <dev.donatello3@gmail.com>
2023-11-29 20:10:10 -0300

    ATEUALIZADO

--- src/index.js
+++ src/index.js
@@ -7,19 +7,10 @@ const server = express();
 
 server.use(express.json());
 
-const port = process.env.PORT || 3000
+
 
 server.get('/', async (req, res) =>{
-try {
-        const carros = await knex('carro');
-        
-        return res.json(carros)
-        
-} catch (error) {
-        console.log(error.message);
-        
-        return res.status(500).json({mensagem: "erro interno server"})}
-     })
+return res.status(200).json({message: "tudo Ok"})
+})
 
-
-server.listen(port)
+server.listen(process.env.PORT)
